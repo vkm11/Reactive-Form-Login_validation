55e4a768a0b96a1db025892bd7731e2ae7937837
vijaykumar <vijay1111mane@gmail.com>
2022-07-20 12:59:43 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormGroup, FormControl, FormControlName} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Reactive-Form';
+  loginForm=new FormGroup({
+    user:new FormControl(), // user:new FormControl('vijay')
+    password:new FormControl(), //password:new FormControl(123)
+  })
+  loginUser(){
+    console.log(this.loginForm.value)
+  }
 }
