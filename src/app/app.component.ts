bee22bf7b787b7d84a52749f051e829702b609d8
vijaykumar <vijay1111mane@gmail.com>
2022-07-20 14:33:23 +0530

    commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, FormControlName} from '@angular/forms'
+import {FormGroup, FormControl, FormControlName, Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +8,20 @@ import {FormGroup, FormControl, FormControlName} from '@angular/forms'
 export class AppComponent {
   title = 'Reactive-Form';
   loginForm=new FormGroup({
-    user:new FormControl(), // user:new FormControl('vijay')
-    password:new FormControl(), //password:new FormControl(123)
+    user:new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), // user:new FormControl('vijay')
+    password:new FormControl('', [Validators.required, Validators.minLength(5),Validators.maxLength(8)]), //password:new FormControl(123)
   })
+
   loginUser(){
     console.log(this.loginForm.value)
   }
+
+  get user(){
+    return this.loginForm.get("user");
+  }
+
+  get password(){
+    return this.loginForm.get('password');
+  }
+
 }
